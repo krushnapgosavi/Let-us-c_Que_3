9a0305cada36f3c56a7d62f11df6f1c39149f085
Krushnagiri Pramodgiri Gosavi <66127129+krushnapgosavi@users.noreply.github.com>
2020-11-25 19:07:47 +0530

    Add files via upload

---
+++ que_3.py
@@ -0,0 +1,8 @@
+m = int(input("Math(/100): "))
+sci = int(input("Science(/100): "))
+eng = int(input("English(/100): "))
+am = m+sci+eng
+per = am*100/300
+print("Total Marks:",am,"/300","\nPercentage:",per)
+
+
